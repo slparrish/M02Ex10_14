179d2e917bb715ce25132c069b112ef36561420f
Scott Parrish <slparrish@gmail.com>
2025-11-06 19:26:40 -0500

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Scott Parrish
+// Ex 10.14
+// 11/06/2025
+// Main.java Class to test and demonstrate the use of the MyDate class.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        MyDate myDate = new MyDate();
+        System.out.println(myDate);
+        long year = 1970 + (MyDate.getCurrentEpoch() / 31556926);
+        System.out.println(MyDate.getCurrentEpoch());
+        long month = (MyDate.getCurrentEpoch() % 31556926) / 2629743;
+        long day = (MyDate.getCurrentEpoch() % 31556926) / 86400;
+        System.out.println("Year: " + year + " Month: " + month + " Day: " + day);
     }
 }
--- src/MyDate.java
+++ src/MyDate.java
@@ -1,4 +1,38 @@
-﻿package PACKAGE_NAME;
-
+// Scott Parrish
+// Ex 10.14
+// 11/06/2025
+// MyDate.java Class to work with dates.
 public class MyDate {
+    private int year = 1970;
+    private int month = 0;      // 12 months 0-11 with 0 representing January
+    private int day = 1;
+
+    public MyDate() {
+    }
+
+    public MyDate(long epoch) {
+
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public static long getCurrentEpoch() {
+        long result = System.currentTimeMillis() / 1000;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Year: " + getYear() + " Month: " + getMonth() + " Day: " + getDay();
+    }
 }
